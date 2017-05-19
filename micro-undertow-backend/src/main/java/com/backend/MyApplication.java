c2f083f8de4b8e6ccf3db31c554c7ee787dbcaf5
armdev <armen.arzumanyan@gmail.com>
2017-05-20 01:45:49 +0400

    updated dependencies

--- micro-undertow-backend/src/main/java/com/backend/MyApplication.java
+++ micro-undertow-backend/src/main/java/com/backend/MyApplication.java
@@ -1,9 +1,8 @@
 package com.backend;
 
-import javax.servlet.ServletException;
-
 import com.backend.core.EmbeddedServer;
 import com.backend.resource.ResourceFactory;
+import javax.servlet.ServletException;
 
 public class MyApplication {
 // url http://localhost:8080/app-name/api/backend/4
